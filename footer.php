6e5e1fae3e943fe025fede80e093c35f0a27adac
jano <alex@worldx3.com>
2024-05-24 17:50:21 +0200

    Working footer.

--- footer.php
+++ footer.php
@@ -1,20 +1,8 @@
-<?php
-
-/**
- * The template for displaying the footer
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
-
-?>
-
 <footer class="footer">
 
 <ul class="list-unstyled">
 	<li><a href="https://www.dropbox.com/s/hne3v5kbrarcy0v/POL%C3%8DTICA%20DE%20PRIVACIDAD-wof.pdf?raw=1">Legal</a></li>
 	<li><a href="https://www.dropbox.com/sh/epzwd8b0afvmz7s/AADfB-Zrhpg5Mf0v1Wlp7nRfa?dl=0&preview=cod_etico_wof_2022_00.pdf">Carta principios</a></li>
-	<!-- <li><a href="https://www.dropbox.com/scl/fo/satm83lvjqfouww8j3de3/h?rlkey=qxtj791qe83j0z7txi2st22nv&dl=0">Prensa</a></li> -->
 </ul>
 
 <?php wp_footer(); ?>
