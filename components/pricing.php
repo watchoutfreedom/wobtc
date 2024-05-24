64a32a701cad2eb3fd5340905ba44d1d7d448acc
jano <alex@worldx3.com>
2024-05-24 18:08:02 +0200

    Working to pricing.

---
+++ components/pricing.php
@@ -0,0 +1,55 @@
+<div class="pricing">
+	<div class="pricing__box">
+		<div class="pricing__card">
+			<div class="pricing__name">Talleres</div>
+			<div class="pricing__euros">59<span>€</span></div>
+			<div class="pricing__day">Viernes 27 sep</div>
+		</div>
+		<ul class="pricing__list">
+			<li>Asiste a los talleres</li>
+			<li>Elige itinerario básico o avanzado</li>
+		</ul>
+	</div>
+	<!-- talleres -->
+
+	<div class="pricing__box">
+		<div class="pricing__card">
+			<div class="pricing__name">Normal</div>
+			<div class="pricing__euros">59<span>€</span></div>
+			<div class="pricing__day">Viernes 27 sep</div>
+		</div>
+		<ul class="pricing__list">
+			<li>Asiste a los talleres</li>
+			<li>Elige itinerario básico o avanzado</li>
+		</ul>
+	</div>
+	<!-- talleres -->
+
+
+	<div class="pricing__box">
+		<div class="pricing__card">
+			<div class="pricing__name">Runing</div>
+			<div class="pricing__euros">59<span>€</span></div>
+			<div class="pricing__day">Viernes 27 sep</div>
+		</div>
+		<ul class="pricing__list">
+			<li>Asiste a los talleres</li>
+			<li>Elige itinerario básico o avanzado</li>
+		</ul>
+	</div>
+	<!-- talleres -->
+
+
+	<div class="pricing__box">
+		<div class="pricing__card">
+			<div class="pricing__name">BIP</div>
+			<div class="pricing__euros">59<span>€</span></div>
+			<div class="pricing__day">Viernes 27 sep</div>
+		</div>
+		<ul class="pricing__list">
+			<li>Asiste a los talleres</li>
+			<li>Elige itinerario básico o avanzado</li>
+		</ul>
+	</div>
+	<!-- talleres -->
+</div>
--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -10,6 +10,7 @@ get_header(); ?>
 <?php get_template_part('components/hero'); ?>
 <?php get_template_part('components/sponsor'); ?>
 <?php get_template_part('components/ponentes'); ?>
+<?php get_template_part('components/pricing'); ?>
 
 
 <?php get_footer(); ?>
