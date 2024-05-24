6bd08a4fb40470f6546c70838b2e9b2b6dacc556
jano <alex@worldx3.com>
2024-05-24 17:48:40 +0200

    Edited hero.

--- components/hero.php
+++ components/hero.php
@@ -1,15 +1,14 @@
 <div class="hero">
 	<div class="hero__content">
 		<div class="hero__header">
-		<div class="hero__logo">logo</div>
-		<div class="hero__meta">27, 28, y 29 de septiembre - Esacio Pavlo VI, Madrid</div>
-	</div>
-	<div class="hero__message">
-		<div class="hero__subtitle">
-			Edición 2024
-	</div>
-	<h1 class="hero__title">
-		Descubre, aprende y comparte en el evento referencia sobre Bitcoin en español
-	</h1>
+			<div class="hero__logo">logo</div>
+			<div class="hero__meta">27, 28, y 29 de septiembre - Esacio Pavlo VI, Madrid</div>
+		</div>
+		<div class="hero__message">
+			<div class="hero__subtitle">Edición 2024</div>
+			<h1 class="hero__title">
+				Descubre, aprende y comparte en el evento referencia sobre Bitcoin en español
+			</h1>
+		</div>
 	</div>
 </div>
