9a1788fbde4416dd115c8a14a89dc445fb3ae664
jano <alex@worldx3.com>
2024-05-25 11:26:52 +0200

    Update last.php

--- components/last.php
+++ components/last.php
@@ -1,7 +1,7 @@
 <div class="last">
   <div class="last__content">
     <div class="last__title">
-      <h2>Edición last</h2>
+      <h2>Edición 2023</h2>
     </div>
     <div class="last__paragraph">
       WOB creció con unas instalaciones únicas y multiplicó las oportunidades de encuentro entre los asistentes. Más de 300 personas se dieron cita en 3 días de actividades que mostraron la riqueza del pensamiento sobre Bitcoin en la comunidad hispana.
