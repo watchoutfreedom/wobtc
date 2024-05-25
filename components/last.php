5b2db52fde5e2a1a5603c7c312d229972053bfb0
jano <alex@worldx3.com>
2024-05-25 13:48:28 +0200

    Update last.php

--- components/last.php
+++ components/last.php
@@ -1,35 +1,37 @@
 <div class="last">
-<div class="last__content">
-    <div class="last__title">
-      <h2>Edición 2023</h2>
+  <div class="last__content">
+      <div class="last__title">
+        <h2>Edición 2023</h2>
+      </div>
+      <div class="last__paragraph">
+        WOB creció con unas instalaciones únicas y multiplicó las oportunidades de encuentro entre los asistentes. Más de 300 personas se dieron cita en 3 días de actividades que mostraron la riqueza del pensamiento sobre Bitcoin en la comunidad hispana.
+      </div>
     </div>
-    <div class="last__paragraph">
-      WOB creció con unas instalaciones únicas y multiplicó las oportunidades de encuentro entre los asistentes. Más de 300 personas se dieron cita en 3 días de actividades que mostraron la riqueza del pensamiento sobre Bitcoin en la comunidad hispana.
+    <div class="last__numbers">
+      <div class="last__box">
+        <div class="last__big">+300</div>
+        <div class="last__small">personas</div>
+      </div>
+      <div class="last__box">
+        <div class="last__big">+20</div>
+        <div class="last__small">speakers</div>
+      </div>
+      <div class="last__box">
+        <div class="last__big">+20</div>
+        <div class="last__small">sponsors</div>
+      </div>
+      <div class="last__box">
+        <div class="last__big">3</div>
+        <div class="last__small">días</div>
+      </div>
     </div>
-  </div>
-  <div class="last__numbers">
-    <div class="last__box">
-      <div class="last__big">+300</div>
-      <div class="last__small">personas</div>
+    <div class="last__bck">
+      <img src="<?php bloginfo('template_directory'); ?>/assets/img/2023.jpg" class="last__img" alt="Fotografia del evento wobitcoin 2023 madrid" />
+      <div class="last__content">
+        <div class="last__paragraph">
+          Con capacidad para más de 600 personas, este año habrá más experiencias prácticas, formativas y oportunidades para establecer lazos de mutuo interés en el mejor ambiente.
+        </div>
+      </div>
     </div>
-    <div class="last__box">
-      <div class="last__big">+20</div>
-      <div class="last__small">speakers</div>
-    </div>
-    <div class="last__box">
-      <div class="last__big">+20</div>
-      <div class="last__small">sponsors</div>
-    </div>
-    <div class="last__box">
-      <div class="last__big">3</div>
-      <div class="last__small">días</div>
-    </div>
-  </div>
-  <div class="last__content last__content--bck">
-    <img src="<?php bloginfo('template_directory'); ?>/assets/img/2023.jpg" class="last__img" alt="Fotografia del evento wobitcoin 2023 madrid" />
-    <div class="last__paragraph">
-      Con capacidad para más de 600 personas, este año habrá más experiencias prácticas, formativas y oportunidades para establecer lazos de mutuo interés en el mejor ambiente.
-    </div>
-  </div>
   </div>
 </div>
