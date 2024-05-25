56fca37c53e82d060b48ffcfececc1392c878d92
jano <alex@worldx3.com>
2024-05-25 13:31:36 +0200

    Update hero.php

--- components/hero.php
+++ components/hero.php
@@ -16,8 +16,8 @@
 			</h1>
 			<div class="hero__btns">
 				<a href="#sponsor" class="hero__link hero__link--arrow"><?php get_template_part('icons/arrow'); ?><span>Saber más</span></a>
-				<a href="" class="hero__link">Entrada en euros</a>
-				<a href="" class="hero__link hero__link--bitcoin">Entrada en ₿</a>
+				<a href="" class="hero__link hero__link--c">Entrada en euros</a>
+				<a href="" class="hero__link hero__link--c hero__link--bitcoin">Entrada en ₿</a>
 			</div>
 		</div>
 	</div>
