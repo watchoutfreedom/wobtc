f05c81277d1019d72fbe3101ad9514f0887fc581
jano <alex@worldx3.com>
2024-05-25 12:58:10 +0200

    Update footer.

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
 <footer class="footer">
-
-	<ul class="list-unstyled">
+	<ul>
 		<li><a href="https://www.dropbox.com/s/hne3v5kbrarcy0v/POL%C3%8DTICA%20DE%20PRIVACIDAD-wof.pdf?raw=1">Legal</a></li>
 		<li><a href="https://www.dropbox.com/sh/epzwd8b0afvmz7s/AADfB-Zrhpg5Mf0v1Wlp7nRfa?dl=0&preview=cod_etico_wof_2022_00.pdf">Carta principios</a></li>
 	</ul>
-
 </footer>
 
 </body>
