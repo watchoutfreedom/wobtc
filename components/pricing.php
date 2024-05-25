c745b2564ae9bb442c216a0bdbb018282e98a435
jano <alex@worldx3.com>
2024-05-25 11:57:02 +0200

    update

--- components/pricing.php
+++ components/pricing.php
@@ -16,11 +16,11 @@
 		<div class="pricing__card">
 			<div class="pricing__name">Normal</div>
 			<div class="pricing__euros">119<span>€</span></div>
-			<div class="pricing__day">Viernes 27 sep</div>
+			<div class="pricing__day">2 días (sábado y domingo)</div>
 		</div>
 		<ul class="pricing__list">
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Elige itinerario básico o avanzado</li>
+			<li><?php get_template_part('icons/check'); ?><?php get_template_part('icons/check'); ?>Debates del sábado y domingo</li>
+			<li><?php get_template_part('icons/check'); ?>Acceso a Fanzone</li>
 		</ul>
 	</div>
 	<!-- talleres -->
@@ -30,11 +30,14 @@
 		<div class="pricing__card">
 			<div class="pricing__name">Runing</div>
 			<div class="pricing__euros">169<span>€</span></div>
-			<div class="pricing__day">Viernes 27 sep</div>
+			<div class="pricing__day">2 días (sábado y domingo)</div>
 		</div>
 		<ul class="pricing__list">
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Elige itinerario básico o avanzado</li>
+		<li><?php get_template_part('icons/check'); ?>Debates del sábado y domingo</li>
+			<li><?php get_template_part('icons/check'); ?>Acceso a Fanzone</li>
+			<li><?php get_template_part('icons/check'); ?>Cocktail del sábado con ponentes! AfterWOB (domingo 15-20 hs)</li>
+			<li><?php get_template_part('icons/check'); ?>Sorteos de entradas, camisetas y más!</li>
+			<li><?php get_template_part('icons/check'); ?>Asientos preferenciales Running</li>
 		</ul>
 	</div>
 	<!-- talleres -->
@@ -44,18 +47,17 @@
 		<div class="pricing__card">
 			<div class="pricing__name">BIP</div>
 			<div class="pricing__euros">379<span>€</span></div>
-			<div class="pricing__day">Viernes 27 sep</div>
+			<div class="pricing__day">3 días (viernes, sábado y domigo)</div>
 		</div>
 		<ul class="pricing__list">
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Asiste a los talleres</li>
-			<li><?php get_template_part('icons/check'); ?>Elige itinerario básico o avanzado</li>
+		<li><?php get_template_part('icons/check'); ?>Acceso a Fanzone</li>
+			<li><?php get_template_part('icons/check'); ?>Cocktail del sábado con ponentes! AfterWOB (domingo 15-20 hs)</li>
+			<li><?php get_template_part('icons/check'); ?>Sorteos de entradas, camisetas y más!</li>
+			<li><?php get_template_part('icons/check'); ?>Asientos preferenciales BIP</li>
+			<li><?php get_template_part('icons/check'); ?>Camiseta WOB 24!</li>
+			<li><?php get_template_part('icons/check'); ?>Acceso a Talleres</li>
+			<li><?php get_template_part('icons/check'); ?>Acceso al Cocktail del viernes con Ponentes</li>
+			<li><?php get_template_part('icons/check'); ?>Comidas en la Fanzone los días sábado y dDomingo</li>
 		</ul>
 	</div>
 	<!-- talleres -->
