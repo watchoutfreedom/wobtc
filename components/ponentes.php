d0f4ab9230d6ac84c2538c92544a3fdec48ff219
jano <alex@worldx3.com>
2024-05-27 13:00:40 +0200

    Scroll test show.

--- components/ponentes.php
+++ components/ponentes.php
@@ -7,7 +7,7 @@
     $ponentes = get_posts($args);
     if ($ponentes): 
       foreach($ponentes as $ponente):  setup_postdata( $ponente );?>
-      <div class="ponentes__card">
+      <div class="ponentes__card anim-up anim-pause-2">
         <div class="ponentes__content">
           <a href="<?php echo get_field("url",$ponente->ID) ?>" class="text-decoration-none 	text-dark" target="_blank">
             <img class="ponentes__img" src="<?php echo get_the_post_thumbnail_url($ponente->ID) ?>" alt="" >
