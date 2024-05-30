4c13214d26dbd964408e20c77d0b5e2ae7893662
jano <alex@worldx3.com>
2024-05-30 16:34:47 +0200

    Update partner.

--- components/partner.php
+++ components/partner.php
@@ -1,26 +1,26 @@
 <div class="sponsor">
-  <?php     
-    $args = array(
-      'post_type' => 'partners',
-      'posts_per_page' => -1
-    );
-    $sponsors = get_posts($args);
-    if ($sponsors): 	
-      foreach($sponsors as $sponsor):  setup_postdata( $sponsor );?>
-    <div class="sponsor__card">
-      <div class="sponsor__content">
-        <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
-          <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($sponsor->ID) ?>" alt="" class="img-fluid">
-        </a>
-          <h5 class="sponsor__title">
-          <h5>
+<?php 	
+  $args = array(
+    'post_type' => 'partner',
+    'posts_per_page' => -1
+  );
+
+  $sponsors = get_posts($args);
+  if ($sponsors): 
+    foreach($sponsors as $sponsor):  setup_postdata( $sponsor );?>
+      <div class="sponsor__card">
+        <div class="sponsor__content">
           <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
-            <?php echo $sponsor->post_title ?>
+            <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($sponsor->ID) ?>" alt="" >
           </a>
-        </h5>
+            <h5 class="sponsor__title">
+               <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
+                <?php echo $sponsor->post_title ?>
+              </a>
+            </h5>
+        </div>
       </div>
-    </div>
-  <?php endforeach;
+    <?php endforeach;
     wp_reset_postdata();
   endif;?>
-  </div>
+</div> <!-- sponsor -->
