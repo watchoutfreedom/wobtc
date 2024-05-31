f353ea2fcfd10d02993992dc3a6783f8ffb256df
jano <alex@worldx3.com>
2024-05-31 12:13:46 +0200

    Update thumb avatar.

--- components/sponsor.php
+++ components/sponsor.php
@@ -5,13 +5,14 @@
     'posts_per_page' => -1
   );
 
+  
   $sponsors = get_posts($args);
   if ($sponsors): 
     foreach($sponsors as $sponsor):  setup_postdata( $sponsor );?>
       <div class="sponsor__card">
         <div class="sponsor__content">
           <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
-            <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($sponsor->ID) ?>" alt="" >
+            <img class="sponsor__img" src="the_post_thumbnail('medium', array( 'class' => 'no-lazy' ))" alt="<?php echo $sponsor->post_title ?>" >
           </a>
             <h5 class="sponsor__title">
                <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
@@ -24,3 +25,7 @@
     wp_reset_postdata();
   endif;?>
 </div> <!-- sponsor -->
+
+<picture class="article-list__picture picture">
+        <?php the_post_thumbnail('medium', array( 'class' => 'no-lazy' )); ?>
+      </picture>
