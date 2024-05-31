1e75503f96e52bea7b2b189815927570f4bca75c
jano <alex@worldx3.com>
2024-05-31 12:26:22 +0200

    Fix sponsor.

--- components/sponsor.php
+++ components/sponsor.php
@@ -5,14 +5,13 @@
     'posts_per_page' => -1
   );
 
-  
   $sponsors = get_posts($args);
   if ($sponsors): 
     foreach($sponsors as $sponsor):  setup_postdata( $sponsor );?>
       <div class="sponsor__card">
         <div class="sponsor__content">
           <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
-            <img class="sponsor__img" src="the_post_thumbnail('medium', array( 'class' => 'no-lazy' ))" alt="<?php echo $sponsor->post_title ?>" >
+            <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($sponsor->ID) ?>" alt="<?php echo $sponsor->post_title ?>" >
           </a>
             <h5 class="sponsor__title">
                <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
@@ -25,7 +24,3 @@
     wp_reset_postdata();
   endif;?>
 </div> <!-- sponsor -->
-
-<picture class="article-list__picture picture">
-        <?php the_post_thumbnail('medium', array( 'class' => 'no-lazy' )); ?>
-      </picture>
