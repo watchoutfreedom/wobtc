e054b8edb2c4616d3ee9e949daf8e91bd5b8695b
jano <alex@worldx3.com>
2024-06-06 18:57:05 +0200

    Edited sponsor.

--- components/sponsor.php
+++ components/sponsor.php
@@ -12,13 +12,11 @@
         <div class="sponsor__content">
           <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
             <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($sponsor->ID) ?>" alt="<?php echo $sponsor->post_title ?>" >
-          </a>
             <h5 class="sponsor__title">
-               <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
                 <?php echo $sponsor->post_title ?>
-              </a>
             </h5>
-        </div>
+            </a>
+            </div>
       </div>
     <?php endforeach;
     wp_reset_postdata();
