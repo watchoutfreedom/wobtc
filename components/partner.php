a1b60853a79eb45ed888ca0a76afdc42509d8dfd
jano <alex@worldx3.com>
2024-06-06 18:55:27 +0200

    Added title to partners.

--- components/partner.php
+++ components/partner.php
@@ -12,6 +12,11 @@
         <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
           <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($sponsor->ID) ?>" alt="<?php echo $sponsor->post_title ?>" class="img-fluid">
         </a>
+        <h5 class="sponsor__title">
+          <a href="<?php echo get_field("url",$sponsor->ID) ?>" class="sponsor__link" target="_blank">
+          <?php echo $sponsor->post_title ?>
+        </a>
+      </h5>
       </div>
     </div>
   <?php endforeach;
