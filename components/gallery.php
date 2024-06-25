35cd0da6e3782c6010ee784f60112a3af4e89c1f
jano <alex@worldx3.com>
2024-06-25 17:25:56 +0200

    Fix gallery.

--- components/gallery.php
+++ components/gallery.php
@@ -1,7 +1,7 @@
 <div class="gallery">
 <div class="gallery__col">
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/01.jpg" alt="Fotografia del evento wobitcoin 2023" />
-    <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/03.jpg" alt="Fotografia del evento wobitcoin 2023" />
+    <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/04.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/07.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/10.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/09.jpg" alt="Fotografia del evento wobitcoin 2023" />
@@ -14,8 +14,8 @@
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/12.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/01.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/02.jpg" alt="Fotografia del evento wobitcoin 2023" />
+    <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/03.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/03.jpg" alt="Fotografia del evento wobitcoin 2023" />
-    <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/04.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/05.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/06.jpg" alt="Fotografia del evento wobitcoin 2023" />
   </div>
@@ -28,6 +28,5 @@
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/04.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/05.jpg" alt="Fotografia del evento wobitcoin 2023" />
     <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/vertical/06.jpg" alt="Fotografia del evento wobitcoin 2023" />
-  <img loading="lazy" src="<?php bloginfo('template_directory'); ?>/assets/photos/horizontal/13.jpg" alt="Fotografia del evento wobitcoin 2023" />
   </div>
 </div>
