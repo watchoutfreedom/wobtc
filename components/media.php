83adb0e9f440e3c77a33a7eaa6c28ca0b85c0619
jano <alex@worldx3.com>
2024-07-06 12:32:01 +0200

    fix

--- components/media.php
+++ components/media.php
@@ -1,27 +1,24 @@
-<div class="ponentes">
-    <?php 
-    $args = array(
-      'post_type' => 'media',
-      'posts_per_page' => -1
-    );
-    $medias = get_posts($args);
-    if ($medias): 
-      foreach($medias as $media):  setup_postdata( $media );?>
-      <div class="ponentes__card">
-        <a class="ponentes__link" href="<?php echo get_field("url",$media->ID) ?>" class="text-decoration-none 	text-dark" target="_blank"></a>            
-        <img class="ponentes__img" src="<?php echo get_the_post_thumbnail_url($media->ID) ?>" alt="<?php echo $media->post_title ?>" >
-        <div class="ponentes__content">
-            <div class="ponentes__title">
-              <h5>
-                <?php echo $media->post_title ?>
-              </h5>
-              <p>
-                <?php echo $media->post_excerpt ?>
-              </p>
+<div class="sponsor">
+<?php 	
+  $args = array(
+    'post_type' => 'media',
+    'posts_per_page' => -1
+  );
+
+  $medias = get_posts($args);
+  if ($medias): 
+    foreach($medias as $media):  setup_postdata( $media );?>
+      <div class="sponsor__card <?php echo get_field("category",$media->ID) ?>">
+        <div class="sponsor__content">
+          <a href="<?php echo get_field("url",$media->ID) ?>" class="sponsor__link" target="_blank">
+            <img class="sponsor__img" src="<?php echo get_the_post_thumbnail_url($media->ID) ?>" alt="<?php echo $media->post_title ?>" >
+            <h5 class="sponsor__title">
+              <?php echo $media->post_title ?>
+            </h5>
+            </a>
             </div>
-        </div>
-    </div>
-      <?php endforeach;
-      wp_reset_postdata();
-    endif;?>
-</div> <!-- media partner -->
+      </div>
+    <?php endforeach;
+    wp_reset_postdata();
+  endif;?>
+</div> <!-- sponsor -->
