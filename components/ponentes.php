1fef3ad25446c19470e211d6c162628baa7a2fa7
jano <alex@worldx3.com>
2024-07-06 12:52:28 +0200

    fix

--- components/ponentes.php
+++ components/ponentes.php
@@ -1,23 +1,22 @@
 <div class="ponentes">
     <?php 
     $args = array(
-      'post_type' => 'media',
-      'category' => 'Silver',
+      'post_type' => 'ponente',
       'posts_per_page' => -1
     );
-    $medias = get_posts($args);
-    if ($medias): 
-      foreach($medias as $media):  setup_postdata( $media );?>
+    $ponentes = get_posts($args);
+    if ($ponentes): 
+      foreach($ponentes as $ponente):  setup_postdata( $ponente );?>
       <div class="ponentes__card">
-        <a class="ponentes__link" href="<?php echo get_field("url",$media->ID) ?>" class="text-decoration-none 	text-dark" target="_blank"></a>            
-        <img class="ponentes__img" src="<?php echo get_the_post_thumbnail_url($media->ID) ?>" alt="<?php echo $media->post_title ?>" >
+        <a class="ponentes__link" href="<?php echo get_field("url",$ponente->ID) ?>" class="text-decoration-none 	text-dark" target="_blank"></a>            
+        <img class="ponentes__img" src="<?php echo get_the_post_thumbnail_url($ponente->ID) ?>" alt="<?php echo $ponente->post_title ?>" >
         <div class="ponentes__content">
             <div class="ponentes__title">
               <h5>
-                <?php echo $media->post_title ?>
+                <?php echo $ponente->post_title ?>
               </h5>
               <p>
-                <?php echo $media->post_excerpt ?>
+                <?php echo $ponente->post_excerpt ?>
               </p>
             </div>
         </div>
