278e1b80cef9d5e76f0838b6c1f0ebc7db7035f7
jano <alex@worldx3.com>
2024-07-29 18:54:28 +0200

    Edited title.

--- components/message.php
+++ components/message.php
@@ -1,8 +1,5 @@
 <div class="last">
   <div class="last__content">
-      <div class="last__title last__title--gray">
-        <h2>Periodo 'Regular'</h2>
-      </div>
       <div class="last__paragraph">
         En 2024 + Encuentros + Debates + Talleres + Micro abierto + Hackaton + Proyectos + Lightning + Brainstorms + Fan Zone + Oportunidad de negocio + Zona entrevistas + Merch + Presentaciones individuales + Arte Bitcoin + Actuaciones + Mucho +
       </div>
