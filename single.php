7ad8cb143988d5fb5dc403fad34a6bc6bfb74349
jano <alex@worldx3.com>
2024-08-08 21:37:24 +0200

    Talleres.

--- single.php
+++ single.php
@@ -3,10 +3,18 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <article class="single">
     <div class="single__meta">
+      <div class="single__info">
+        <span><?php the_field('field_66a224bc1299d'); ?></span>
+        <?php the_field('field_66a224971299b'); ?>
+      </div>
       <h1 class="single__title"><?php the_title(); ?></h1>
     </div>
+    <div class="single__description">
+      <?php the_field('field_66a224e77d91a'); ?>
+    </div>
+
     <div class="single__content main">
-    <?php the_content("Sigue leyendo"); ?>     
+      <?php the_content("Sigue leyendo"); ?>     
     </div>
   
 <?php endwhile; else: ?> 
