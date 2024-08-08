b0f2e043fffd85ba5d73637662755b6c54988bb4
jano <alex@worldx3.com>
2024-08-08 22:21:02 +0200

    Edited menu.

--- components/message.php
+++ components/message.php
@@ -1,8 +1,10 @@
 <div class="last">
-  <div class="last__content">
-      <div class="last__paragraph">
-        En 2024 + Encuentros + Debates + Talleres + Micro abierto + Hackaton + Proyectos + Lightning + Brainstorms + Fan Zone + Oportunidad de negocio + Zona entrevistas + Merch + Presentaciones individuales + Arte Bitcoin + Actuaciones + Mucho +
-      </div>
-    </div>
+  <div class="last__menu">
+    <a href="#portada">Portada</a>
+		<a href="#ponentes">Ponentes</a>
+		<a href="#entradas">Entradas</a>
+		<a href="#sponsor">Sponsors</a>
+		<a href="#faq">FAQs</a>
+		<a href="#2023">Evento23</a>
   </div>
 </div>
