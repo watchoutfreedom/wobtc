83f13bf7a5dbb76cb0fdf5b88a50aaee5492fd56
jano <alex@worldx3.com>
2024-09-13 12:13:03 +0200

    Add programa to menu.

--- components/message.php
+++ components/message.php
@@ -2,6 +2,7 @@
     <a href="#portada">Portada</a>
 		<a href="#ponentes">Ponentes</a>
 		<a href="#entradas">Entradas</a>
+		<a href="#programa">Programa</a>
 		<a href="#sponsor">Sponsors</a>
 		<a href="#faq">FAQs</a>
 		<a href="#2023">Evento23</a>
