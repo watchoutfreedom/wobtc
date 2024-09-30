0cdfd8de7870fea55406e067cb5d56b2f1b36519
watchoutfreedom <watchoutfreedom@gmail.com>
2024-09-30 14:00:21 +0200

    wob

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -29,16 +29,6 @@ get_header(); ?>
 </div>
 <?php get_template_part('components/pricing'); ?>
 
-<div class="heading" id="red">
-	<h2 class="heading__title">Publicaciones en la Red Wob!</h2>
-	<div class="heading__btns">
-		<!-- <a href="https://docs.google.com/forms/d/e/1FAIpQLSe0-fwY0o65VI3YDfPkOtYn4jp-ye0hslGhrkKQboI3wfaC1A/viewform" target="_blank">Quiero ser ponente</a> -->
-	</div>
-</div>
-
-<div class="ponentes">
-<iframe src="https://help.wobitcoin.org/es/Embbed/9" width="100%" height="800px" overflow="scroll" frameborder="0"></iframe>
-</div>
 
 <div class="heading"  id="programa">
 	<h2 class="heading__title">Programa 2024</h2>
