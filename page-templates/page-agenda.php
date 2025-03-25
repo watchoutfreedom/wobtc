c67bc926a4aca3b9820db044d55b6bfb4fd939cf
jano <alex@worldx3.com>
2025-03-25 19:42:28 +0100

    New tables

--- page-templates/page-agenda.php
+++ page-templates/page-agenda.php
@@ -6,7 +6,7 @@
 get_header(); ?>
 <div class="wrap">
   <h1>Agenda example</h1>
-  <h2>Viernes, 27 de septiembre</h2>
+  <h2 class="day-title">Viernes, 27 de septiembre</h2>
   <?php 
     $rows = get_field('viernes');
 
@@ -31,6 +31,61 @@ get_header(); ?>
         echo '</ul>';
     }
     ?>
+
+
+<h2 class="day-title">Sábado, 28 de septiembre</h2>
+  <?php 
+    $rows = get_field('sabado');
+
+    if( $rows ) {
+        echo '<ul class="day">';
+        foreach( $rows as $row ) {
+            $hora = $row['hora'];
+            $titulo = $row['titulo']; 
+            $descripcion = $row['descripcion']; 
+            echo '<li>';
+                if( !empty($hora) ) {
+                    echo '<div class="time">' . esc_html( $hora ) . '</div>';
+                }
+                if( !empty($titulo) ) {
+                    echo '<div class="content"><h3>' . esc_html( $titulo ) . '</h3>'; 
+                }
+                if( !empty($descripcion) ) {
+                    echo '<p><strong>Descripción:</strong> ' . esc_html( $descripcion ) . '</p></div>'; 
+                }
+            echo '</li>';
+        }
+        echo '</ul>';
+    }
+    ?>
+
+
+<h2 class="day-title">Domingo, 29 de septiembre</h2>
+  <?php 
+    $rows = get_field('domingo');
+
+    if( $rows ) {
+        echo '<ul class="day">';
+        foreach( $rows as $row ) {
+            $hora = $row['hora'];
+            $titulo = $row['titulo']; 
+            $descripcion = $row['descripcion']; 
+            echo '<li>';
+                if( !empty($hora) ) {
+                    echo '<div class="time">' . esc_html( $hora ) . '</div>';
+                }
+                if( !empty($titulo) ) {
+                    echo '<div class="content"><h3>' . esc_html( $titulo ) . '</h3>'; 
+                }
+                if( !empty($descripcion) ) {
+                    echo '<p><strong>Descripción:</strong> ' . esc_html( $descripcion ) . '</p></div>'; 
+                }
+            echo '</li>';
+        }
+        echo '</ul>';
+    }
+    ?>
+
 </div>
 
 <?php get_footer(); ?>
