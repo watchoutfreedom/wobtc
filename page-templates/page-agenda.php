e562dc8ac5b2cd594bea59dd756ee2b0782c3d01
jano <alex@worldx3.com>
2025-03-25 19:25:12 +0100

    fix agenda

--- page-templates/page-agenda.php
+++ page-templates/page-agenda.php
@@ -8,25 +8,23 @@ get_header(); ?>
   <h1>Agenda example</h1>
   <h2>Viernes, 27 de septiembre</h2>
   <?php 
-// Obtener el grupo de campos llamado 'viernes'
-$grupo_campos = get_field('viernes');
+    $grupo_campos = get_field('field_67e2f16d40617');
 
-if ($grupo_campos): ?>
-    <ul class="campos-grupo">
-        <?php 
-        // Verificar y mostrar los campos dentro del grupo
-        if (!empty($grupo_campos['field_67e2f17840618'])): ?>
-            <li><?php echo esc_html($grupo_campos['field_67e2f17840618']); ?></li>
-        <?php endif; ?>
-        
-        <?php if (!empty($grupo_campos['field_67e2f19a40619'])): ?>
-            <li><?php echo esc_html($grupo_campos['field_67e2f19a40619']); ?></li>
-        <?php endif; ?>
-        
-        <?php if (!empty($grupo_campos['field_67e2f1a44061a'])): ?>
-            <li><?php echo esc_html($grupo_campos['field_67e2f1a44061a']); ?></li>
-        <?php endif; ?>
-    </ul>
+    if ($grupo_campos): ?>
+        <ul class="campos-grupo">
+            <?php 
+            if (!empty($grupo_campos['field_67e2f17840618'])): ?>
+                <li><?php echo esc_html($grupo_campos['field_67e2f17840618']); ?></li>
+            <?php endif; ?>
+            
+            <?php if (!empty($grupo_campos['field_67e2f19a40619'])): ?>
+                <li><?php echo esc_html($grupo_campos['field_67e2f19a40619']); ?></li>
+            <?php endif; ?>
+            
+            <?php if (!empty($grupo_campos['field_67e2f1a44061a'])): ?>
+                <li><?php echo esc_html($grupo_campos['field_67e2f1a44061a']); ?></li>
+            <?php endif; ?>
+        </ul>
 <?php endif; ?>
 
 </div>
