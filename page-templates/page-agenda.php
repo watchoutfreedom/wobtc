16c7382c5830929bd64ea52877e139a6af6d5a14
jano <alex@worldx3.com>
2025-03-25 19:13:21 +0100

    Update agenda

--- page-templates/page-agenda.php
+++ page-templates/page-agenda.php
@@ -6,6 +6,31 @@
 get_header(); ?>
 <div class="wrap">
   <h1>Agenda example</h1>
+  <h2>Viernes, 27 de septiembre</h2>
+  <?php
+  if (have_posts()) :
+      while (have_posts()) : the_post();
+          $campo1 = get_field('field_67e2f17840618'); // Campo 1
+          $campo2 = get_field('field_67e2f19a40619'); // Campo 2
+          $campo3 = get_field('field_67e2f1a44061a'); // Campo 3
+
+          if ($campo1) {
+              echo '<p>Campo 1: ' . esc_html($campo1) . '</p>';
+          }
+
+          if ($campo2) {
+              echo '<p>Campo 2: ' . esc_html($campo2) . '</p>';
+          }
+
+          if ($campo3) {
+              echo '<p>Campo 3: ' . esc_html($campo3) . '</p>';
+          }
+      endwhile;
+  else :
+      echo '<p>No se encontraron publicaciones.</p>';
+  endif;
+  ?>
+  
 </div>
 
 <?php get_footer(); ?>
