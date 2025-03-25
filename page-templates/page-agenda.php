ec5af7e8f8e925c8739b45d8819083e6849f2b95
jano <alex@worldx3.com>
2025-03-25 19:30:54 +0100

    fix

--- page-templates/page-agenda.php
+++ page-templates/page-agenda.php
@@ -9,12 +9,12 @@ get_header(); ?>
   <h2>Viernes, 27 de septiembre</h2>
 
   <?php 
-$rows = get_field('field_67e2f16d40617'); // Reemplaza 'repeater_field_name' con el nombre de tu campo repetidor
+$rows = get_field('viernes'); // Reemplaza 'repeater_field_name' con el nombre de tu campo repetidor
 
 if( $rows ) {
     echo '<ul class="slides">'; // Clase opcional para la lista
     foreach( $rows as $row ) {
-        $texto = $row['field_67e2f17840618']; // Reemplaza 'texto' con el nombre del subcampo donde guardas el texto
+        $texto = $row['hora']; // Reemplaza 'texto' con el nombre del subcampo donde guardas el texto
         echo '<li>';
             echo esc_html( $texto ); // Muestra el texto de manera segura
         echo '</li>';
