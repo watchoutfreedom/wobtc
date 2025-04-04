aede0aec47d669171bdbe3b00b5e3606e6f8b64a
jano <alex@worldx3.com>
2025-04-04 15:31:04 +0200

    Update

--- components/hero.php
+++ components/hero.php
@@ -24,13 +24,14 @@
 			<div class="hero__subtitle hero__subtitle">
 
 				<span>
-
-							<?php 
-			$grupo = the_field('field_67efd9320d32f'); 
-			if ($grupo) {
-					echo esc_html($grupo['field_67efd93f0d330']);
-			}
-			?>
+				<?php 
+$grupo = get_field('field_67efd9320d32f'); // Obtiene el grupo de campos, NO imprime
+if ($grupo && isset($grupo['field_67efd93f0d330'])) { 
+    echo esc_html($grupo['field_67efd93f0d330']); // Imprime el campo de texto correcto
+} else {
+    echo "Campo vacío o no encontrado.";
+}
+?>
 				</span>
 			</div>
 			<h1 class="hero__title">
